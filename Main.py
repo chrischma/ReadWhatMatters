c104718ad6521d6caf82579b9631edd36e9065bb
Chris <post@christoph-schmalfuss.de>
2021-03-24 17:52:40 +0100

    Updated GUI

--- Main.py
+++ Main.py
@@ -12,19 +12,12 @@ def input_file_dialog():
     print('check')
     global input_file, file_path
     input_file = None
-    while input_file is None:
-        try:
-            file_path = '/Users/Chris/Downloads/10.1515_zrs-2016-0021.pdf'
-            input_file = PyPDF2.PdfFileReader(file_path)
-            file_path = filedialog.askopenfilename(filetypes=[('pdf file', '*.pdf')])
 
-            input_file = PyPDF2.PdfFileReader(file_path)
+    file_path = '/Users/Chris/Downloads/10.1515_zrs-2016-0021.pdf'
+    input_file = PyPDF2.PdfFileReader(file_path)
+    file_path = filedialog.askopenfilename(filetypes=[('pdf file', '*.pdf')])
 
-        except FileNotFoundError:
-            print('File not found. Please try again.')
-
-        except PyPDF2.utils.PdfReadError:
-            print('Please use a valid .pdf-file!')
+    input_file = PyPDF2.PdfFileReader(file_path)
 
 
 def short_pdf():
@@ -58,17 +51,25 @@ def short_pdf():
 
 
 root = Tk()
-root.geometry("250x250")
 root.title("Read What Matters 0.1")
+root.geometry("330x250")
 input_file_dialog_button = Button(root, text="Chose file", command=input_file_dialog)
 start_processing_button = Button(root, text="Go", command=short_pdf)
-welcome_text = Label(root, text="Enter your search-term here:")
+l0 = Label(root, text="Welcome! Let's save some time! ;-)")
+l1 = Label(root, text="Enter any search-term: ")
+l2 = Label(root, text="Chose any pdf-file:")
+logo = PhotoImage(file="logo.gif")
+logo_panel = Label(root, image=logo)
+
 search_term_input_field = Entry(root)
 search_term_input_field.insert(0, "Literatur")
 
-welcome_text.pack()
-search_term_input_field.pack()
-input_file_dialog_button.pack()
-start_processing_button.pack()
+
+logo_panel.grid(row=0, column=2, sticky="w", pady="30")
+l1.grid(row=1, column=1)
+l2.grid(row=2, column=1)
+search_term_input_field.grid(row=1, column=2)
+input_file_dialog_button.grid(row=2, column=2, sticky="W")
+start_processing_button.grid(row=3, column=2, sticky="w")
 
 mainloop()
